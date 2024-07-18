69ff58067c8cacc3b747a49248d637bda6e20be4
aa-florence <okosunuzflorence@gmail.com>
2024-07-18 13:13:53 -0400

    WIP: Create Book rating

--- handlers/rating_handlers.go
+++ handlers/rating_handlers.go
@@ -1,1 +1,29 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/fokosun/go-rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+var rating models.Rating
+
+func CreateRating(c *gin.Context) {
+	// Bind the JSON input to the struct
+	if err := c.ShouldBindJSON(&rating); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Validate the Rating
+    err := rating.Validate()
+    if err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"validation_error": err.Error()})
+		return
+    }
+
+	//ensure the given book id exists
+	//set the user id from gin context
+	//create the rating
+}
--- models/rating.go
+++ models/rating.go
@@ -1,9 +1,19 @@
 package models
 
+import (
+	"github.com/go-playground/validator/v10"
+)
+
 type Rating struct {
 	ID      uint `gorm:"primaryKey"`
 	UserID  int
-	BookID  int
-	Rating  int    `gorm:"default:1"`
+	BookID  int `json:"book_id" validate:"required"`
+	Rating  int `gorm:"default:1"`
 	Comment string `json:"title"`
+}
+
+// Validate validates the Rating fields.
+func (u *Rating) Validate() error {
+	validate := validator.New()
+	return validate.Struct(u)
 }
