6bb55aaea93b40f11d740e19d6ac2fa29c90bc7e
aa-florence <okosunuzflorence@gmail.com>
2024-07-29 10:23:45 -0400

    chore: wip

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"context"
-	"log"
-
 	"github.com/fokosun/go-rest-api/config"
 	"github.com/fokosun/go-rest-api/models"
 	"github.com/fokosun/go-rest-api/routes"
 )
 
-var ctx = context.Background()
+// var ctx = context.Background()
 
 func main() {
 	Init()
@@ -28,11 +25,11 @@ func Init() {
 	db.AutoMigrate(&models.Rating{})
 
 	// Add check constraint for Rating field
-	db.Exec("ALTER TABLE ratings ADD CONSTRAINT check_rating CHECK (rating IN (1, 2, 3, 4, 5))")
+	// db.Exec("ALTER TABLE ratings ADD CONSTRAINT check_rating CHECK (rating IN (1, 2, 3, 4, 5))")
 
 	// Set a key-value pair
-	err := config.Client.Set(ctx, "key", "value", 0).Err()
-	if err != nil {
-		log.Fatalf("Error setting key: %v", err)
-	}
+	// err := config.Client.Set(ctx, "key", "value", 0).Err()
+	// if err != nil {
+	// 	log.Fatalf("Error setting key: %v", err)
+	// }
 }
